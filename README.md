7b9c041f4312e26e85a6cf573e865fc823f0b7c3
杨卓 <destroy314@outlook.com>
2023-04-07 11:54:11 +0000

    初始提交

---
+++ README.md
@@ -0,0 +1,26 @@
+# maskrcnn_ros
+
+[Maskrcnn](https://github.com/BIT1136/Maskrcnn) 的 ROS 包装器.
+
+## 安装
+
+    git clone --recurse-submodules
+
+## 安装依赖
+
+使用 conda/mamba 创建虚拟环境并安装依赖:
+
+    conda create -n maskrcnn_ros python=3.10 pytorch=1.13 torchvision numpy
+
+可能兼容更旧的环境.
+
+将 `nodes/node.py` 第一行中的 Python 解释器路径修改为安装了依赖的解释器.
+
+## 添加模型
+
+将模型文件置于 `models` 文件夹中,修改 `nodes/node.py` 中的 `model_path` 变量.
+
+## 运行节点
+
+    roslaunch maskrcnn_ros node.launch
+    
---
+++ nodes/node.py
@@ -0,0 +1,51 @@
+#!/root/mambaforge/envs/grcnn/bin/python
+
+import time
+
+import torch
+import numpy as np
+
+import rospy
+import ros_numpy
+from sensor_msgs.msg import Image
+
+from maskrcnn_ros.srv import InstanceSeg, InstanceSegResponse
+from segmentation import Segmentation
+
+
+def seg_callback(req):
+    global seg
+    img: np.ndarray = ros_numpy.numpify(req.input)
+    shape = img.shape
+    rospy.loginfo(f"开始实例分割 {shape}")
+    t_start = time.perf_counter()
+
+    img = np.transpose(img, (2, 0, 1))
+    img = img.astype(np.float32) / 255
+    img = np.clip(img - img.mean(), -1, 1)
+    _, predict_classes, _, predict_masks = seg.predict(img)
+    label_mask = np.zeros(shape[:2], dtype=np.uint8)
+    mask_threshold = 0.5
+    num_instances = len(predict_classes)
+    for i in range(num_instances):
+        index = np.where(predict_masks[i] > mask_threshold)
+        label_mask[index] = i + 1
+
+    t_end = time.perf_counter()
+    rospy.loginfo(f"实例分割完成,检测到 {num_instances} 个实例,耗时 {(t_end - t_start)*1000:.2f}ms")
+
+    res = InstanceSegResponse()
+    res.seg = ros_numpy.msgify(Image, label_mask, encoding="mono8")
+    return res
+
+
+if __name__ == "__main__":
+    model_path = "../models/model_19.pth"
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    rospy.logdebug(f"加载实例分割网络 {model_path}")
+    seg = Segmentation(model_path, device)
+
+    rospy.init_node("maskrcnn_ros_server")
+    s = rospy.Service("/maskrcnn_ros_server/seg_instance", InstanceSeg, seg_callback)
+    rospy.loginfo("/maskrcnn_ros_server 节点就绪")
+    rospy.spin()
